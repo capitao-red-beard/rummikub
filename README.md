7349feb5b028aaa5bd5044517f5a6c8afe08eafd
capitao-red-beard <jasper.v.d.valk@gmail.com>
2019-08-13 12:56:45 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rummikub
+
+This is a simply Rummikub game made in Python, I would like to create it to be played in the terminal.
---
+++ tile.py
@@ -0,0 +1,20 @@
+from collections import namedtuple
+
+Tile = namedtuple('Tile', ['value', 'color'])
+
+class RummikubTile:
+    values = [str(n) for n in range(1, 14)]
+    colors = 'red blue orange black'.split()
+
+    def __init__(self):
+        self._tiles = [Tile(value, color) for color in self.colors
+                                          for value in self.values]
+
+    def __len__(self):
+        return len(self._tiles)
+    
+    def __getitem__(self, position):
+        return self._tiles[position]
+
+rummikub_tiles = RummikubTile()
+print(rummikub_tiles._tiles)
